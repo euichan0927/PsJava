c5c64cf5a901443773dd6720d9c0650baaf38d9f
euichan0927 <yuichan99@naver.com>
2024-01-26 16:42:27 +0900

    Weekly Quiz 1 - 문자열로 변환 commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+C
+ChangeToString.java,9\9\999078cf3af4a4da4a8983d6be81ea976082314e
---
+++ ChangeToString.java
@@ -0,0 +1,8 @@
+public class ChangeToString {
+	class Solution {
+		public String solution(int n) {
+			String answer = String.valueOf(n);
+			return answer;
+		}
+	}
+}
